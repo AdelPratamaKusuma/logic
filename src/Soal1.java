10d5dd95661a79e07e477bcf50d72439c9a02940
AdelPratamaKusuma <adelpratamakusuma@gmail.com>
2023-11-21 09:20:49 +0700

    logic

---
+++ src/Soal1.java
@@ -0,0 +1,15 @@
+public class Soal1 {
+    public static void main(String[] args) {
+    Soal1(10);
+    }
+
+    public static void Soal1(int n){
+        int angka = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.println(angka+"\t");
+            angka+=2;
+        }
+
+    }
+
+}
